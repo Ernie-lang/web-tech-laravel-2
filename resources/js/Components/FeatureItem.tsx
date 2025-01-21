26527c295009043cac98d2934ed5cc5a73ca407e
Ernie-lang <abols.ernests@gmail.com>
2025-01-21 19:42:36 +0200

    Upvoting and Downvoting Features

--- resources/js/Components/FeatureItem.tsx
+++ resources/js/Components/FeatureItem.tsx
@@ -2,6 +2,7 @@ import { Feature } from "@/types";
 import { useState } from "react";
 import { Link } from "@inertiajs/react";
 import FeatureActionsDropdown from "./FeatureActionsDropdown";
+import FeatureUpvoteDownvote from "./FeatureUpvoteDownvote";
 
 export default function  FeatureItem({feature}: {feature: Feature}) {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -13,23 +14,7 @@ export default function  FeatureItem({feature}: {feature: Feature}) {
     return (
         <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800 min-h-44">
             <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
-                <div className="flex flex-col items-center">
-                    <button className={feature.user_has_upvoted ? 'text-amber-600' : ''}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-                    </svg>
-                    </button>
-                    <span className={`text-2xl font-semibold ${
-                        feature.user_has_upvoted || feature.user_has_downvoted ? 'text-amber-600' : ''}`}>
-                        {feature.upvote_count}
-                    </span>
-                    <button className={feature.user_has_downvoted ? 'text-amber-600' : ''}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                    </svg>
-                    </button>
-
-                </div>
+                <FeatureUpvoteDownvote feature={feature} />
                 <div className="flex-1">
                     <h2 className="text-2xl mb-2">
                         <Link href={route('feature.show', feature)}>
