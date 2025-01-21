962f91decc275627fae68dec4ef2720a08fec7a6
Ernie-lang <abols.ernests@gmail.com>
2025-01-21 23:45:43 +0200

    Implementing backend functionality for user types

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -4,17 +4,15 @@ import { Head } from '@inertiajs/react';
 export default function Dashboard() {
     return (
         <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Dashboard
-                </h2>
-            }
+            
         >
             <Head title="Dashboard" />
 
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
+                            This is just a page to let you know that you have logged in successfully.
+                            You are welcome to stay on this page, but all the fun stuff is on the Features page!
+                            
                         </div>
                     </div>
         </AuthenticatedLayout>
